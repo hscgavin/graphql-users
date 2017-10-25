8f91867fdab5abdfd4f649bd488c2cdda4c48c5f
Gavin He <hscgavin@gmail.com>
2017-10-26 09:12:54 +1100

    Replace hardcoded data with json-server

--- schema/schema.js
+++ schema/schema.js
@@ -1,41 +1,38 @@
 const graphql = require('graphql');
+const fetch = require('node-fetch');
 
 const {
-    GraphQLObjectType,
-    GraphQLString,
-    GraphQLInt,
-    GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
 } = graphql;
 
-
-const users = [
-    {id: '23', firstName: 'Bill', age: 21},
-    {id: '47', firstName: 'Samantha', age: 22}
-]
+const apiHost = 'http://localhost:3000';
 
 const UserType = new GraphQLObjectType({
-    name: 'User',
-    fields: {
-        id: {type: GraphQLString},
-        firstName: {type: GraphQLString},
-        age: {type: GraphQLInt},
+  name: 'User',
+  fields: {
+    id: {type: GraphQLString},
+    firstName: {type: GraphQLString},
+    age: {type: GraphQLInt},
 
-    }
+  }
 });
 
 const RootQuery = new GraphQLObjectType({
-   name: 'RootQueryType',
-    fields: {
-       user: {
-           type: UserType,
-           args: {id: {type: GraphQLString}},
-           resolve(parentValue, args) {
-               return users.find(user => user.id === args.id)
-           }
-       }
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: {id: {type: GraphQLString}},
+      resolve(parentValue, args) {
+        return fetch(`${apiHost}/users/${args.id}`).then(res => res.json())
+      }
     }
+  }
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+  query: RootQuery
 })
