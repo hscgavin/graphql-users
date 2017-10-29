b79728cc9d104e4b24f22ba41d93ae0a338221ac
Gavin He <hscgavin@gmail.com>
2017-10-29 23:55:16 +1100

    Resolve users in company

--- schema/schema.js
+++ schema/schema.js
@@ -6,22 +6,29 @@ const {
   GraphQLString,
   GraphQLInt,
   GraphQLSchema,
+  GraphQLList,
 } = graphql;
 
 const apiHost = 'http://localhost:3000';
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: {type: GraphQLString},
     name: {type: GraphQLString},
     description: {type: GraphQLString},
-  }
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return fetch(`${apiHost}/companies/${parentValue.id}/users`).then(res => res.json());
+      }
+    }
+  })
 });
 
 const UserType = new GraphQLObjectType({
   name: 'User',
-  fields: {
+  fields: () => ({
     id: {type: GraphQLString},
     firstName: {type: GraphQLString},
     age: {type: GraphQLInt},
@@ -31,7 +38,7 @@ const UserType = new GraphQLObjectType({
         return fetch(`${apiHost}/companies/${parentValue.companyId}`).then(res => res.json());
       }
     }
-  }
+  })
 });
 
 const RootQuery = new GraphQLObjectType({
